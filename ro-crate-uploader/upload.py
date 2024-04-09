998f2fafddea440bbd72a376cf0c1d7c68744cb9
Eli Chadwick <eli.chadwick.256@gmail.com>
2024-04-09 17:48:25 +0100

    break code into functions

--- ro-crate-uploader/upload.py
+++ ro-crate-uploader/upload.py
@@ -4,6 +4,7 @@ from zenodo_client import Creator, Metadata, ensure_zenodo
 
 
 def build_zenodo_creator_list(authors: list[Person] | Person) -> list[Creator]:
+    """Given an RO-Crate author or list of authors, build a list of "creators" to use in Zenodo upload."""
     if isinstance(authors, list):
         return [Creator(name=a["name"]) for a in authors]
     else:
@@ -11,39 +12,54 @@ def build_zenodo_creator_list(authors: list[Person] | Person) -> list[Creator]:
         return [Creator(name=authors["name"])]
 
 
-# load a crate
-crate_path = "test/test-data/crate1"
-crate = ROCrate(crate_path)
+def build_zenodo_metadata_from_crate(crate: ROCrate) -> Metadata:
+    """Given an RO-Crate, collect the metadata to use in Zenodo upload"""
+    # retrieve author(s)
+    authors = crate.root_dataset.get("author")
+    creators = build_zenodo_creator_list(authors)
 
-# retrieve author(s)
-authors = crate.root_dataset.get("author")
-creators = build_zenodo_creator_list(authors)
+    # retrieve title
+    title = crate.root_dataset.get("name")
+    description = crate.root_dataset.get("description")
 
-# retrieve title
-title = crate.root_dataset.get("name")
-description = crate.root_dataset.get("description")
+    # Define the metadata that will be used on initial upload
+    data = Metadata(
+        title=title,
+        upload_type="dataset",
+        description=description,
+        creators=creators,
+    )
 
-# generate zip file
-zip_path = f"{crate_path}.zip"
-zipped = crate.write_zip(zip_path)
-print(zipped)
+    return data
 
-# Define the metadata that will be used on initial upload
-data = Metadata(
-    title=title,
-    upload_type="dataset",
-    description=description,
-    creators=creators,
-)
 
-# res = ensure_zenodo(
-#     key="ro-crate-uploader",  # this is a unique key you pick that will be used to store
-#     # the numeric deposition ID on your local system's cache
-#     data=data,
-#     paths=[
-#         zip_path,
-#     ],
-#     sandbox=True,  # remove this when you're ready to upload to real Zenodo
-# )
+def ensure_crate_zipped(crate: ROCrate) -> str:
+    """Returns a path to the zipped crate."""
+    zip_path = f"/tmp/crate.zip"
+    zipped = crate.write_zip(zip_path)
+    return zipped
 
-# print(res.json())
+
+def upload_crate_to_zenodo(crate_zip_path: str, metadata: Metadata):
+    res = ensure_zenodo(
+        key="ro-crate-uploader",  # this is a unique key you pick that will be used to store
+        # the numeric deposition ID on your local system's cache
+        data=metadata,
+        paths=[
+            zip_path,
+        ],
+        sandbox=True,  # remove this when you're ready to upload to real Zenodo
+    )
+
+    return res.json()
+
+
+if __name__ == "__main__":
+    crate_path = "test/test-data/crate1"
+    crate = ROCrate(crate_path)
+
+    metadata = build_zenodo_metadata_from_crate(crate)
+    crate_zip_path = ensure_crate_zipped(crate)
+    print(crate_zip_path, metadata)
+    # record = upload_crate_to_zenodo(crate_zip_path, metadata)
+    # print(record)
