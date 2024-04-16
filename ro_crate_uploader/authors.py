daeae8de7635fe1bc3a4b6b0664b9e1186c4a2dc
Eli Chadwick <eli.chadwick.256@gmail.com>
2024-04-16 16:43:29 +0100

    pull author details from rocrate Person into zenodo Creator

--- ro_crate_uploader/authors.py
+++ ro_crate_uploader/authors.py
@@ -14,20 +14,26 @@ ORCID_REGEX = r"https:\/\/orcid\.org\/(?P<id>([0-9]{4}-){3}[0-9]{3}[0-9X])"
 def build_zenodo_creator_list(authors: list[Person] | Person) -> list[Creator]:
     """Given an RO-Crate author or list of authors, build a list of "creators"
     to use in Zenodo upload."""
-    if isinstance(authors, list):
-        return [Creator(name=a["name"]) for a in authors]
-    else:
-        # single author
-        return [Creator(name=authors["name"])]
+    if not isinstance(authors, list):
+        authors = [authors]
+    return [Creator(**get_author_details(a)) for a in authors]
 
 
 def get_author_details(person: Person) -> dict:
     """Collects details from a Person entity and returns them using Creator fields"""
     # check if @id is an ORCID
-    raise NotImplementedError
     id = person["@id"]
-    if id:
-        pass
+    orcid_id = get_orcid_id_or_none(id)
+
+    # if no name present, fall back on id
+    # TODO handle case where only a name is provided as @id but rocrate adds a #
+    name = person.get("name", id)
+
+    affiliation = person.get("affiliation", None)
+    if affiliation:
+        affiliation = str(affiliation)
+
+    return {"name": name, "orcid": orcid_id, "affiliation": affiliation}
 
 
 def get_orcid_id_or_none(str: str) -> str | None:
