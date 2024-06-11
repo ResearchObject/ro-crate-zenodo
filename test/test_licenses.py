5806d67b661ed7444f0f3772e6ddbd8c6514a55c
Eli Chadwick <eli.chadwick@manchester.ac.uk>
2024-06-11 17:45:07 +0100

    clean up

--- test/test_licenses.py
+++ test/test_licenses.py
@@ -1,30 +1,11 @@
-from unittest import TestCase
-
 import json
-import requests
 import pytest
 
-from rocrate.model.person import Person
-from rocrate.rocrate import ROCrate, ContextEntity
+from rocrate.rocrate import ContextEntity
 
 from rocrate_upload.upload import (
-    build_zenodo_creator_list,
-    build_zenodo_metadata_from_crate,
     get_license,
 )
-
-SPDX_LICENSES = requests.get(
-    "https://raw.githubusercontent.com/spdx/license-list-data/main/json/licenses.json"
-).json()["licenses"]
-
-ZENODO_LICENSES = requests.get(
-    "https://raw.githubusercontent.com/zenodo/zenodo-rdm/e477db5dad2c35edd3e6549c8725e1d67fab4ba2/app_data/vocabularies/licenses.csv",
-).content
-ZENODO_LICENSES = str(ZENODO_LICENSES)
-
-SPDX_LICENSES_IN_ZENODO = [
-    l for l in SPDX_LICENSES if f'{l["licenseId"].lower()},' in ZENODO_LICENSES
-]
 
 with open("test/test_data/licenses.json") as licenses_file:
     LICENSES_TO_TEST = json.load(licenses_file)["licenses"]
